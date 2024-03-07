12b68ba9414c6e9eb0c9df748d3d75cc8c21ba3c
DanieleM <danielemarchio709@gmail.com>
2024-03-07 02:00:01 +0100

    add:esercizio terminato

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+const nome = prompt('Inserisci il tuo nome') //string||null
+const cognome = prompt('Inserisci il tuo cognome') //string||null
+const colore = prompt('Inserisci il tuo colore preferito') //string||null
+let min = 1;
+let max = 100;
+let key= Math.floor(Math.random() * (max - min + 1)) + min; //numero random
+const password = (nome + cognome + colore + key) //string
+console.log(password)
