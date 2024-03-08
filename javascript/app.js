a6873fe77989a5fe1dd94b7c4f91e9867ca32769
DanieleM <danielemarchio709@gmail.com>
2024-03-08 01:15:48 +0100

    ok

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,5 @@ const colore = prompt('Inserisci il tuo colore preferito') //string||null
 let min = 1; // number
 let max = 100; // number
 let key= Math.floor(Math.random() * (max - min + 1)) + min; //numero random
-const password = (nome + cognome + colore + key) //string
+const password = nome + cognome + colore + key //string
 console.log(password)
